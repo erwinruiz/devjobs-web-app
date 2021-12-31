01bdea3670bb1ac4d3d66aad84024bd72db50b62
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-12-31 12:56:41 -0800

    Add Search Filter component (mobile design 375 px)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Fragment } from "react";
 import Header from "./Components/Header";
+import Home from "./Pages/Home";
 
 function App() {
   return (
+    <Fragment>
       <Header />
+      <main>
+        <Home />
+      </main>
+    </Fragment>
   );
 }
 
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import SearchFilter from "../Components/SearchFilter";
+
+function Home() {
+  return (
+    <Fragment>
+      <SearchFilter />
+    </Fragment>
+  );
+}
+
+export default Home;
